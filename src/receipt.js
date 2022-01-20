a3cfa8e9a2599e8852bc1d04a41748e2209bac88
Gaurav Ahuja <gaurav.ahuja@unthinkable.co>
2022-01-20 17:55:19 +0530

    Showing progress message in case on non-card transactions

--- src/receipt.js
+++ src/receipt.js
@@ -18,37 +18,46 @@ const Receipt = ({ response }) => {
 
   if (!response || !Object.keys(response).length) return null;
 
-  return (
-    <>
-      <h3>Payment receipt</h3>
-      <table>
-        <tr>
-          <td>Transaction ID</td>
-          <td>{response?.charge?.id}</td>
-        </tr>
-        <tr>
-          <th>Name</th>
-          <th>Value</th>
-        </tr>
-        <tr>
-          <td>Status</td>
-          <td>{response?.status_details?.status}</td>
-        </tr>
-        <tr>
-          <td>Amount</td>
-          <td>{amount}</td>
-        </tr>
-        <tr>
-          <td>Email</td>
-          <td>{email}</td>
-        </tr>
-        <tr>
-          <td>Description</td>
-          <td>{response?.charge?.description}</td>
-        </tr>
-      </table>
-    </>
-  );
+  if (response?.status_details?.status === 'success') {
+    return (
+      <>
+        <h3>Payment receipt</h3>
+        <table>
+          <tr>
+            <td>Transaction ID</td>
+            <td>{response?.charge?.id}</td>
+          </tr>
+          <tr>
+            <th>Name</th>
+            <th>Value</th>
+          </tr>
+          <tr>
+            <td>Status</td>
+            <td>{response?.status_details?.status}</td>
+          </tr>
+          <tr>
+            <td>Amount</td>
+            <td>{amount}</td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>{email}</td>
+          </tr>
+          <tr>
+            <td>Description</td>
+            <td>{response?.charge?.description}</td>
+          </tr>
+        </table>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h3>Hi.</h3><br></br>
+        <h4>Your transaction has been processed from our end. Please visit the store to complete the transaction.</h4>
+      </>
+    );
+  }
 }
 
 export default Receipt;
