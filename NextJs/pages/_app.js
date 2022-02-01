0cc3994093ccdafb3b589d75f31992abb1acd735
ali-cash <ali@holacash.mx>
2022-02-01 11:55:03 -0600

    updated next project

--- NextJs/pages/_app.js
+++ NextJs/pages/_app.js
@@ -1,13 +1,15 @@
-import '../styles/globals.css'
-import {Fragment} from "react";
+import "../styles/globals.css";
+import { Fragment } from "react";
+import styles from "../App.css";
+import "../node_modules/react-bootstrap/dist/";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-      <Fragment>
-        <Component {...pageProps} />
-      </Fragment>
-
-  )
+    <Fragment>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
